2f4bbbbef02f15ea54d534059a9c22fff31c0a06
Maryia Kastenka <kastenka.whatsup@gmail.com>
2021-11-28 12:32:14 +0300

    Add new program from week1

---
+++ CourseraPython/week1/FindDiscriminant.py
@@ -0,0 +1,22 @@
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+x1 = x2 = 0
+
+Discriminant = b**2 - 4*a*c
+if Discriminant < 0:
+    print("Нет корней!!!")
+elif Discriminant == 0:
+    x1 = (-b + Discriminant**0.5)/(2*a)
+    print(f"Корень один: {x1}")
+else:
+    x1 = (-b + Discriminant**0.5)/(2*a)
+    print(int(x1))
+    x2 = (-b - Discriminant**0.5)/(2*a)
+    print(int(x2))
+
+
+
+
