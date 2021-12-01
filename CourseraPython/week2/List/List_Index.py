6371a84081e2bc54f9a40fb432510976e8bf13aa
Kastenka <mashakastenka@gmail.com>
2021-12-01 09:51:50 +0300

    Add files via upload

---
+++ CourseraPython/week2/List/List_Index.py
@@ -0,0 +1,16 @@
+# Списки. Примеры использования методов
+
+# Метод index - возвращает индекс (позицию элемента) в последовательности
+num_list = list(range(5))
+print(num_list)  # [0, 1, 2, 3, 4]
+
+print(num_list.index(2))  # 2
+print(num_list.index(1, 0, 4))  # 1
+print(num_list.index(6)) # ValueError: 6 is not in list
+
+
+
+
+
+
+
