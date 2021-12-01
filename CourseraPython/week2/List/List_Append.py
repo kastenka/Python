9791262fc2d280aab5120beaa1d8fc133cd8a677
Kastenka <mashakastenka@gmail.com>
2021-12-01 09:06:05 +0300

    Add files via upload

---
+++ CourseraPython/week2/List/List_Append.py
@@ -0,0 +1,9 @@
+# Списки. Примеры использования методов
+# Метод append
+num_list = list(range(5))
+print(num_list)  # [0, 1, 2, 3, 4]
+num_list.append(5)
+print(num_list)  # [0, 1, 2, 3, 4, 5]
+
+# метод append возвращает None
+print(num_list.append(6))  # None
