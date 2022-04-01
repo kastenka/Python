f5ba1d94c610b0e242c5a0d0a643601ff37dbe2a
Kastenka <mashakastenka@gmail.com>
2022-04-01 11:43:29 +0300

    Create class Parser

--- PythonPractice/Parsing/Parsing_Onliner_by.py
+++ PythonPractice/Parsing/Parsing_Onliner_by.py
@@ -1,50 +1,46 @@
 from bs4 import BeautifulSoup
-import urllib.request as request
+import urllib.request
 
 
-def parse_site(url):
-    req = request.urlopen(url)
-    html = req.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    parse_object = soup.find_all('div', class_='news-tidings__item')
-    return parse_object
+class Parser:
 
+    raw_html = ''
+    html = ''
+    results = []
 
-def write_to_file(func):
-    def wrapped(data, filename='results.txt'):
-        result = func(data)
-        with open(filename, 'w', encoding='utf-8') as file:
-            for index, item in enumerate(result):
+    def __init__(self, url, file_path='results.txt'):
+        self.url = url
+        self.file_path = file_path
+
+    def get_html(self):
+        req = urllib.request.urlopen(self.url)
+        self.raw_html = req.read()
+        self.html = BeautifulSoup(self.raw_html, 'html.parser')
+
+    def save_to_file(self):
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            for index, item in enumerate(self.results):
                 file.write(f"{index+1}) {item['title']}\n"
                            f"Description: {item['description']}\n"
                            f"Link: {item['link']}"
                            f"\n\n")
-        return result
-    return wrapped
 
+    def parsing(self):
+        news = self.html.find_all('div', class_='news-tidings__item')
 
-@write_to_file
-def get_data(url):
-    results = []
-    news = parse_site(url)
+        for index, item in enumerate(news):
+            title = item.find('span', class_='news-helpers_show_mobile-small')
+            if title:  # check if there
+                description = item.find('div', class_='news-tidings__speech')
+                link = item.a
+                self.results.append({
+                    "title": title.get_text(strip=True),
+                    "description": description.get_text(strip=True),
+                    "link": link.get('href')
+                })
 
-    for index, item in enumerate(news):
-        title = item.find('span', class_='news-helpers_show_mobile-small')
-        if title:  # check if there
-            description = item.find('div', class_='news-tidings__speech')
-            link = item.a
-            results.append({
-                "title": title.get_text(strip=True),
-                "description": description.get_text(strip=True),
-                "link": link.get('href')
-            })
-    return results
+    def run(self):
+        self.get_html()
+        self.parsing()
+        self.save_to_file()
 
-
-def main():
-    url = 'https://tech.onliner.by/tag/it-belarus'
-    parse_data = get_data(url)
-
-
-if __name__ == "__main__":
-    main()
